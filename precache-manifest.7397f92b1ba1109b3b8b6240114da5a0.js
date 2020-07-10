b08cde8dac18da7540399eab0e2a68df5d31ea42
amir vansover <amir@vansover.com>
2020-07-11 02:00:56 +0300

    Updates

---
+++ precache-manifest.7397f92b1ba1109b3b8b6240114da5a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af9876b3fa9b7dc6943de269e21a5d6",
+    "url": "/cardgame/index.html"
+  },
+  {
+    "revision": "9c82181daa398078de6a",
+    "url": "/cardgame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "78d3eaa2a4c00f23c2f2",
+    "url": "/cardgame/static/js/2.47fb844e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cardgame/static/js/2.47fb844e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c82181daa398078de6a",
+    "url": "/cardgame/static/js/main.cae8fef8.chunk.js"
+  },
+  {
+    "revision": "4ba5c00a78d22e562741",
+    "url": "/cardgame/static/js/runtime-main.85c7a9f0.js"
+  }
+]);
